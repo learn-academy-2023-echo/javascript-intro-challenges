d8658f40e0b3e47269e3d77b8a98d83003831517
mikechavo <mchavez76@yahoo.com>
2023-07-19 16:44:49 -0700

    my turn

--- conditionals-justin-michael.js
+++ conditionals-justin-michael.js
@@ -19,14 +19,26 @@
 // // Does "LEARN" contain the subset "RN"?
 //     console.log("LEARN".includes("RN"))
 // Does "LEARN" contain the subset "rn"?
+  console.log("LEARN".includes("rn"))
 // Does "LEARN"[0] strictly equal "l"?
+  console.log("LEARN"[0] === "l")
 // Modify the code from the previous question to return true.
+  console.log("LEARN"[0] === "L")
 // Make sure you try different options and change the variables to ensure properly working code.
-
+  console.log("LEARN"[4] == "n") //???
+  console.log("LEARN"[4] == "N") //???
 // Write a statement that takes a variable of item and logs "in budget" if a price is $100 or less.
-
+var money = 80
+  if(money < 100) {
+    console.log("in budget")
+  }
 // Write a statement that takes a variable of hungry and logs "eat food" if you are hungry and "keep coding" if you are not hungry.
-
+let hungry = true
+  if(hungry = true) { 
+    console.log("eat foood")
+  } else {
+    console.log("keep coding")
+  }
 // Write a statement that takes a variable of trafficLight and logs "go" if the light is green, "slow down" if the light is yellow and "stop" if the light is red.
 
 // Write a statement that takes two variables that are numbers and outputs the larger number. If the numbers are equal, output "the numbers are the same".

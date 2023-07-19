cec1791d3034b9433ff1dc0c6c038d6ef46a74be
j-felder <ajustinfelder@gmail.com>
2023-07-19 11:45:51 -0700

    the next few items

--- javascrtipt-intro-challenges-justin-michael.js
+++ javascrtipt-intro-challenges-justin-michael.js
@@ -10,9 +10,14 @@
 // Write the code that will log the outcome of 45 divided by 5.
     console.log(45 / 5)
 // Write the code that will log the outcome of 5 to the power of 7.
+    console.log(5 ** 7)
 // Write the code that will log the whole number remainder of 33 divided by 6.
+    console.log( 33 % 6)
 // Write the code that will log the length of a string containing your name.
+var mySuperHeroName = "The Flash"
+    console.log(mySuperHeroName.length)
 // Write the code that will log whether your string includes the letter "e"?
+    console.log(mySuperHeroName.includes("e"))
 // Write the code that will log the character at the first index of the string.
 // Write the code that will log the string in all uppercase letters.
 // Consider the variables:

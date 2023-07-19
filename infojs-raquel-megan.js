fdfd4494e034a84446b22132fdbac98542b03c7d
megandee <meganderisi@gmail.com>
2023-07-19 13:30:47 -0500

    part one of challenge completed

---
+++ infojs-raquel-megan.js
@@ -0,0 +1,21 @@
+console.log(34+71)
+
+console.log(67-123)
+
+console.log(56*23)
+
+console.log(45 / 5)
+
+console.log(5**7)
+
+console.log(33%6)
+
+let yourName="Megan"
+
+console.log(yourName.length)
+
+console.log(yourName.includes ("e"))
+
+console.log(yourName.charAt (1))
+
+console.log(yourName.toUpperCase ())

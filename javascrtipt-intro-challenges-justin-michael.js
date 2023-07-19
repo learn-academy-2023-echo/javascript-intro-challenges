584e3682abcec2cd53477a9fdbaa41b12f78fd06
mikechavo <mchavez76@yahoo.com>
2023-07-19 11:37:50 -0700

    update

--- javascrtipt-intro-challenges-justin-michael.js
+++ javascrtipt-intro-challenges-justin-michael.js
@@ -6,7 +6,9 @@
 // Write the code that will log the outcome of 67 subtracted from 123.
     console.log(123 - 67)
 // Write the code that will log the outcome of 56 multiplied by 23.
+    console.log(56 * 23)
 // Write the code that will log the outcome of 45 divided by 5.
+    console.log(45 / 5)
 // Write the code that will log the outcome of 5 to the power of 7.
 // Write the code that will log the whole number remainder of 33 divided by 6.
 // Write the code that will log the length of a string containing your name.

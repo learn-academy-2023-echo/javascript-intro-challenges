9be90032b1e3bb26a52087f1938dc024d254c629
Kiana-Massey <kxm7046@mavs.uta.edu>
2023-07-20 11:58:42 -0700

    second commit

--- arrays-rk-km.js
+++ arrays-rk-km.js
@@ -1,41 +1,40 @@
-//  var groceryList = ["chips", "dip", "cookies"]
-// // Write the code that will add "soda" to the end of the original array.
-// console.log(groceryList.push("soda"))
-// console.log(groceryList)
-// // Write the code that will add "granola" to the end of array without altering the original array.
-// var granola = "granola"
-// console.log(groceryList.concat(granola))
+ var groceryList = ["chips", "dip", "cookies"]
+ // Write the code that will add "soda" to the end of the original array.
+console.log(groceryList.push("soda"))
+console.log(groceryList)
+// Write the code that will add "granola" to the end of array without altering the original array.
+var granola = "granola"
+console.log(groceryList.concat(granola))
 
 // Write the code that will return a subset of the array containing only "chips" and "dip".
-// var chipAnddip = groceryList.slice(0,2)
-// console.log(chipAnddip)
+var chipAnddip = groceryList.slice(0,2)
+console.log(chipAnddip)
 // Write the code that will add "beans" to the "chips" and "dip" array.
-// var beans = "beans"
-// console.log(chipAnddip.concat(beans))
+var beans = "beans"
+console.log(chipAnddip.concat(beans))
 
-
-// var numbers = [2, 4, 6, 8, 10]
+ var numbers = [2, 4, 6, 8, 10]
 // // Write the code that will add the number 0 to the beginning of the array.
-// console.log(numbers.unshift(0))
-// console.log(numbers)
+ console.log(numbers.unshift(0))
+ console.log(numbers)
 // // Write the code that will add the number 12 to the end of the array.
-// console.log(numbers.push(12))
-// console.log(numbers)
+ console.log(numbers.push(12))
+ console.log(numbers)
 // // Write the code that will remove the first number from the array.
-// console.log(numbers.shift())
-// console.log(numbers)
+ console.log(numbers.shift())
+ console.log(numbers)
 // // Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
-// var numbersTwo = [0]
-// console.log(numbersTwo.concat(numbers))
+ var numbersTwo = [0]
+ console.log(numbersTwo.concat(numbers))
 
 
-// var numSet = [2, 13, 6, 8, 4, 2]
-// // Write the code that finds the index of the first appearance of the number 2.
-// console.log(numSet.indexOf(2))
-// // Write the code that finds the index of the last appearance of the number 2.
-// console.log(numSet.lastIndexOf(2))
-// // Write the code that returns the number at the third index.
-// console.log(numSet.slice(3,4))
+ var numSet = [2, 13, 6, 8, 4, 2]
+ // Write the code that finds the index of the first appearance of the number 2.
+ console.log(numSet.indexOf(2))
+// Write the code that finds the index of the last appearance of the number 2.
+ console.log(numSet.lastIndexOf(2))
+// Write the code that returns the number at the third index.
+console.log(numSet.slice(3,4))
 
 
 var characters = ["y", "a", "r", "r", "a"]

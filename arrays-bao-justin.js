42d8ec1c2de2bffec39f55d4546ebf0d0c8df804
j-felder <ajustinfelder@gmail.com>
2023-07-20 11:39:24 -0700

    another one

--- arrays-bao-justin.js
+++ arrays-bao-justin.js
@@ -24,7 +24,7 @@ var groceryList = ["chips", "dip", "cookies"]
 // console.log(groceryList)
 
 // Write the code that will add "beans" to the "chips" and "dip" array.
-console.log(groceryList.slice(0, 2).concat(array = ["beans"]))
+// console.log(groceryList.slice(0, 2).concat(array = ["beans"]))
 
 //-------------------------------
 
@@ -32,22 +32,33 @@ console.log(groceryList.slice(0, 2).concat(array = ["beans"]))
 
 var numbers = [2, 4, 6, 8, 10]
 // Write the code that will add the number 0 to the beginning of the array.
-
+    // numbers.unshift(0)
+    // console.log(numbers)
 // Write the code that will add the number 12 to the end of the array.
-
+    // numbers.push(12)
+    // console.log(numbers)
 // Write the code that will remove the first number from the array.
+    // numbers.shift()
+    // console.log(numbers)  // this works!
 
+    // console.log(shift(numbers)) //no good. shift needs to be after for syntex purposes
+    
 // Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
+
+let bestNumber = [0]
+let creativeEnough = bestNumber.concat(numbers) 
+// console.log(creativeEnough) // works
 //-------------------------------
 
 // Consider the variable:
 
 var numSet = [2, 13, 6, 8, 4, 2]
 // Write the code that finds the index of the first appearance of the number 2.
-
+//  console.log(numSet.indexOf(2))
 // Write the code that finds the index of the last appearance of the number 2.
-
+//  console.log(numSet.lastIndexOf(2))
 // Write the code that returns the number at the third index.
+//  console.log(numSet[2])
 //-------------------------------
 
 // Consider the variable:
@@ -70,7 +81,10 @@ var characters = ["y", "a", "r", "r", "a"]
 // Consider the variables:
 
 var numbers = [42, 221, 71, 7, 18, 87]
-// var oddIndexes = []
+var oddIndexes = []
 // Write the code that logs the values from the numbers array that are at odd indexes.
-
+console.log(numbers[1], numbers[3], numbers[5])
+// console.log(oddIndexes)
 // Write the code that adds the values from odd indexes into the oddIndexes array.
+oddIndexes.push(numbers[1], numbers[3], numbers[5])
+console.log(oddIndexes)

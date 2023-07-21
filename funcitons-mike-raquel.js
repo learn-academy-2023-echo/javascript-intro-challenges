edffb6da3d13e17278d5832eaa972ac84db9cbdd
mikechavo <mchavez76@yahoo.com>
2023-07-21 11:34:23 -0700

    functions update

---
+++ funcitons-mike-raquel.js
@@ -0,0 +1,83 @@
+// 💻 Challenges
+// Copy the challenges into your JavaScript file. Comment out the instructions and code the solution to each problem beneath the prompt. Each solution requires pseudocode. Test your functions with multiple function calls.
+
+// Write a function named marco that returns "polo".
+// input - marco
+// output - polo
+// const marco = () => {
+//     return "polo"
+// }
+//   var change = marco()
+//   console.log(change)
+// Write a function named greeting that takes a name as an argument and returns "Welcome, <person's name here>!"
+// input - greeting, name
+// output - string, name
+// const greeting = (name) => {
+//   return `Welcome, ${name}!`
+// }
+//   console.log(greeting("Mike"))
+// Write a function named oddOrEven that takes a number as an argument and returns whether the number is odd or even.
+// input - oddOrEven, num
+// output - odd or even
+// const oddOrEven = (num) => {
+//   if (num % 2 === 0) {
+//     return "even"
+//   } else {
+//     return "odd"
+//   }
+// }
+// var num1 = 10
+// var num2 = 3
+
+// console.log(oddOrEven(num1))
+// console.log(oddOrEven(num2))
+// Write a function named triple that takes a number as an argument and returns the result of that number multiplied by 3.
+//input - triple(num)
+//output - num * 3
+// const triple = (num) => {
+//   return num * 3
+// }
+// console.log(triple(3))
+// Write a function named multiply that takes two numbers as arguments and returns the result of the numbers multiplied together.
+// input - multiply(num1,num2)
+// output - num1 * num2
+// const multiply = (num1, num2) => {
+//   return num1 * num2
+// }
+// console.log(multiply(2, 4))
+// Write a function named divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
+// input - divibleBy(num1, num2)
+//output - num1 / num2
+// const divibleBy = (num1, num2) => {
+//   if (num1 % num2 === 0) {
+//     return `${num1} is evenly divisible by ${num2}`
+// } else {
+//     return `${num1} is not evenly divisible by ${num2}'
+//   }
+//   }
+//   const divibleBy = (num1, num2) => {
+//     if (num1 % num2 === 0) {
+//       return `${num1} is evenly divisible by ${num2}`
+//     } else {
+//       return `${num1} is not evenly divisble by ${num2}`
+//     }
+//   }
+// console.log(divibleBy(10, 5))
+// console.log(divibleBy(5, 10))
+// Write a function named assignGrade that takes a number score as an argument and returns the letter grade for the score.
+
+// Write a function named isLonger that takes two strings as arguments and returns the string that contains the most characters.
+
+// Write a function named greaterNum that takes two numbers as arguments and returns whichever number is the greater (higher) number.
+
+// Write a function named yelling that takes a string as an argument and return the string in all uppercase case letters.
+
+// 🏔 Stretch Goals
+// The World Translator
+
+// Write a function named helloWorld that takes a language code (e.g. "es", "de", "en") as an argument and returns "Hello World!" in the given language. Ensure you function works for at least 5 languages.
+// Have your function default to returning English.
+// The Pluralizer
+
+// Write a function named pluralizer that takes a number and a singular noun as arguments and returns the number and pluralized form of the noun, if necessary.
+// Enhance your function so it can handle a few collective nouns like "sheep", "goose", "child", "person" and "species".

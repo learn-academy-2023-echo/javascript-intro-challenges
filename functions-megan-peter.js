4b3cf53f6169357b0298c032a6310e19494b4bc4
megandee <meganderisi@gmail.com>
2023-07-21 13:06:48 -0500

    part one of funcyions challenge

---
+++ functions-megan-peter.js
@@ -0,0 +1,57 @@
+// Write a function named marco that returns "polo".
+// const marco = () => {
+//     return "polo"
+// } 
+// console.log(marco())
+
+// Write a function named greeting that takes a name as an argument and returns "Welcome, <person's name here>!"
+// const greeting = (name) => {        
+//     return `Welcome, ${name}!`
+// }
+// console.log(greeting("Peter"))
+
+// Write a function named oddOrEven that takes a number as an argument and returns whether the number is odd or even.
+// const oddOrEven = (number) => {
+//     if(number % 2 === 0){
+//         return `${number} is even`
+//     } else {
+//         return `${number} is odd`
+//     }
+// }
+// console.log(oddOrEven(5))
+
+// Write a function named triple that takes a number as an argument and returns the result of that number multiplied by 3.
+// const triple = (number) => {
+//     return number * 3 
+// }
+// console.log(triple(5))
+
+// Write a function named multiply that takes two numbers as arguments and returns the result of the numbers multiplied together.
+// const multiply =(num1, num2) => {
+//     return num1 * num2
+// }
+// console.log(multiply(5, 2))
+
+// Write a function named divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
+// const divisibleBy =(num1, num2) => {
+//     if (num1 % num2 === 0){
+//         return `${num1} is evenly divisisible by ${num2}`
+//     } else {
+//         return `${num1} is not evenly divisisible by ${num2}`
+//     }
+// }
+// console.log(divisibleBy(28, 34))
+
+// Write a function named assignGrade that takes a number score as an argument and returns the letter grade for the score.
+
+// Write a function named isLonger that takes two strings as arguments and returns the string that contains the most characters.
+
+// Write a function named greaterNum that takes two numbers as arguments and returns whichever number is the greater (higher) number.
+
+// Write a function named yelling that takes a string as an argument and return the string in all uppercase case letters.
+
+// 🏔 Stretch Goals
+// The World Translator
+
+// Write a function named helloWorld that takes a language code (e.g. "es", "de", "en") as an argument and returns "Hello World!" in the given language. Ensure you function works for at least 5 languages.
+// Have your function default to returning English.
